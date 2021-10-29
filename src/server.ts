9a6b8ea657d55f1a8c5526bbba6f238e1da21117
Kyle Smith <kylesmith090502@gmail.com>
2021-10-28 20:24:20 -0400

    Feature: Adds header and is preflight ready
    This also changed the listening port as 3000 will be used for front-end

--- src/server.ts
+++ src/server.ts
@@ -4,16 +4,34 @@ import { IIncomingMessageWithBody, ReadMessage } from './shared';
 export const startServer = () => {
 	const server = createServer(async (req: IIncomingMessageWithBody, res) => {
 		
+		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+		res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
+		res.setHeader('Accept', 'application/json; charset=utf-8')
+		res.setHeader('Content-Type', 'application/json; charset=utf-8')
+		if (req.method == "OPTIONS") {
+			res.statusCode = 200;
+			res.write(JSON.stringify({message: 'Preflight request approved.'}))
+			return res.end();
+		}
 		
-		const requestWithMessage = await ReadMessage(req);
+		let streamEnded = false;
+		const requestWithMessage = await ReadMessage(req).catch(() => {
+			res.statusCode = 400;
+			res.write(JSON.stringify({message: 'Body must be in JSON format'}))
+			streamEnded = true;
+			return res.end();
+		});
+		// @ts-ignore
+		if (streamEnded === true) return;
 		if (typeof requestWithMessage === 'object') req.body = requestWithMessage;
 		
 		await Router(req, res);
-	
+		
 		return;
-	}).listen(3000);
+	}).listen(8080);
 
-	console.log('Server Created...');
+	console.log('Server Created... URL: http://localhost:8080');
 	
 	return server;
 }
